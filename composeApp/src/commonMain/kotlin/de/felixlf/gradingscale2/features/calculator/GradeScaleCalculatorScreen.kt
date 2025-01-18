ca2c1ebc2930f626e37f5ee4a292b0f75b5011ce
Felix Leyva <leyva@adesso-mobile.de>
2025-01-18 20:26:11 +0100

    main: Improved calculator text fields

--- composeApp/src/commonMain/kotlin/de/felixlf/gradingscale2/features/calculator/GradeScaleCalculatorScreen.kt
+++ composeApp/src/commonMain/kotlin/de/felixlf/gradingscale2/features/calculator/GradeScaleCalculatorScreen.kt
@@ -1,18 +1,29 @@
 package de.felixlf.gradingscale2.features.calculator
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.selectAll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import de.felixlf.gradingscale2.entities.util.MockGradeScalesGenerator
@@ -51,58 +62,104 @@ private fun GradeScaleCalculatorScreen(
 ) {
     val gradeScale = remember(uiState.selectedGradeScale) { uiState.selectedGradeScale }
     Column(
-        modifier = Modifier.fillMaxSize().background(color = MaterialTheme.colorScheme.onPrimary),
-        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
     ) {
         GradeScaleDropboxSelector(
             gradeScalesNames = uiState.gradeScalesNamesWithId.map { it.gradeScaleName }.toImmutableList(),
             selectedGradeScaleName = uiState.selectedGradeScale?.gradeScaleName,
             onSelectGradeScale = onSelectGradeScale,
-            modifier = Modifier.weight(1f),
+            modifier = Modifier.fillMaxWidth(),
             defaultText = stringResource(Res.string.gradescale_list_select_grade_scale),
         )
 
-        val totalPointsState = textFieldManager(uiState.totalPoints?.stringWithDecimals() ?: "") {
-            onSetTotalPoints(it.toDoubleOrNull() ?: 1.0)
+        HorizontalDivider()
+        if (gradeScale == null) {
+            // TODO: modify this text to be a string resource
+            Text(text = "No grade scale selected")
+        } else {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.SpaceBetween,
+            ) {
+                val totalPointsState = textFieldManager(uiState.totalPoints?.stringWithDecimals() ?: "") {
+                    onSetTotalPoints(it.toDoubleOrNull() ?: 1.0)
+                }
+
+                CalculatorTextField(
+                    modifier = Modifier.padding(16.dp),
+                    state = totalPointsState,
+                    label = "Total Points",
+                )
+
+                val pointState = textFieldManager(uiState.currentGrade?.points?.stringWithDecimals() ?: "") {
+                    onSetPoints(it.toDoubleOrNull() ?: 0.0)
+                }
+
+                CalculatorTextField(
+                    modifier = Modifier.padding(16.dp),
+                    state = pointState,
+                    label = "Points",
+                )
+
+                val percentageState = textFieldManager((uiState.currentPercentage)?.times(100)?.stringWithDecimals() ?: "") {
+                    onSetPercentage((it.toDoubleOrNull()?.div(100)) ?: 0.0)
+                }
+
+                CalculatorTextField(
+                    modifier = Modifier.padding(16.dp),
+                    state = percentageState,
+                    label = "Percentage",
+                )
+
+                val gradeNameState = textFieldManager(uiState.currentGrade?.namedGrade ?: "") {
+                    onSelectGradeName(it)
+                }
+
+                CalculatorTextField(
+                    modifier = Modifier.padding(16.dp),
+                    state = gradeNameState,
+                    label = "Grade Name",
+                )
+            }
         }
-
-        BasicTextField(
-            modifier = Modifier.weight(1f).padding(16.dp),
-            state = totalPointsState,
-        )
-
-        val pointState = textFieldManager(uiState.currentGrade?.points?.stringWithDecimals() ?: "") {
-            onSetPoints(it.toDoubleOrNull() ?: 0.0)
-        }
-
-        BasicTextField(
-            modifier = Modifier.weight(1f).padding(16.dp),
-            state = pointState,
-        )
-
-        val percentageState = textFieldManager(uiState.currentPercentage?.stringWithDecimals() ?: "") {
-            onSetPercentage(it.toDoubleOrNull() ?: 0.0)
-        }
-
-        BasicTextField(
-            modifier = Modifier.weight(1f).padding(16.dp),
-            state = percentageState,
-        )
-
-        val gradeNameState = textFieldManager(uiState.currentGrade?.namedGrade ?: "") {
-            onSelectGradeName(it)
-        }
-
-        BasicTextField(
-            modifier = Modifier.weight(1f).padding(16.dp),
-            state = gradeNameState,
-        )
     }
-    HorizontalDivider()
-    if (gradeScale == null) {
-        Text(text = "No grade scale selected")
-        return
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun CalculatorTextField(
+    modifier: Modifier = Modifier,
+    state: TextFieldState,
+    label: String? = null,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    val isFocused by interactionSource.collectIsFocusedAsState()
+    LaunchedEffect(isFocused) {
+        if (isFocused) state.edit { selectAll() }
     }
+
+    BasicTextField(
+        modifier = modifier,
+        state = state,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Decimal,
+        ),
+        textStyle = MaterialTheme.typography.titleLarge.copy(
+            color = MaterialTheme.colorScheme.onSurface,
+        ),
+        interactionSource = interactionSource,
+        decorator = { innerTextField ->
+            OutlinedTextFieldDefaults.DecorationBox(
+                value = state.text.toString(),
+                innerTextField = innerTextField,
+                enabled = true,
+                singleLine = true,
+                label = { label?.let { Text(it) } },
+                interactionSource = interactionSource,
+                visualTransformation = VisualTransformation.None,
+            )
+        },
+    )
 }
 
 @Preview
