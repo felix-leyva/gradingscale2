a7c363ef25df545e6e1dfe7c323de433c7a27e35
Felix Leyva <felix.leyva@adesso.de>
2025-05-23 11:06:17 +0200

    main: feat: Update navigation rail width handling to use wrapContentWidth for improved layout adaptability

--- composeApp/src/commonMain/kotlin/de/felixlf/gradingscale2/scaffold/PersistentScaffold.kt
+++ composeApp/src/commonMain/kotlin/de/felixlf/gradingscale2/scaffold/PersistentScaffold.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -86,7 +87,7 @@ private inline fun ScaffoldState.NavigationRailScaffold(
         content = {
             Box(
                 modifier = Modifier
-                    .widthIn(max = 80.dp)
+                    .wrapContentWidth()
                     .zIndex(2f),
             ) {
                 navigationRail()
